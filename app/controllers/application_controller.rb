f6758ac4b1f2a039234ff686959cc83844d45d41
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-26 21:59:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,7 +55,7 @@ class ApplicationController < Sinatra::Base
     new_params[:content] = params[:content]
     old_article.update(new_params)
     
-    redirect '/articles/2'
+    redirect '/articles/#{id}'
   end
   
   delete '/articles/:id' do 
